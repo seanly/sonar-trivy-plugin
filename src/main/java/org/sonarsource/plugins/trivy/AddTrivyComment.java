3553d99a4d76c55becdc156d3029fdc335a0bc2b
YS Liu <seanly@opsbox.dev>
2025-07-13 21:58:25 +0800

    refactor: remove unused imports from AddTrivyComment
    - Remove unused Map and HashMap imports
    - Clean up import statements for better code organization

--- src/main/java/org/sonarsource/plugins/trivy/AddTrivyComment.java
+++ src/main/java/org/sonarsource/plugins/trivy/AddTrivyComment.java
@@ -13,8 +13,6 @@ import java.io.StringWriter;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Map;
-import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
